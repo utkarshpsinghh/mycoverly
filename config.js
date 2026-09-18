ea3626dfd136d1717a9afc3a9ed0b81b54cc09e2
Utkarsh Pratap Singh <utkarshetw2006@gmail.com>
2026-09-19 02:58:08 +0530

    update

--- config.js
+++ config.js
@@ -1,2 +1,13 @@
-/* Public storefront configuration. Replace these placeholders before launch. */
-window.COVERLY_CONFIG={MERCHANT_UPI_ID:'YOUR_UPI_ID_HERE',MERCHANT_NAME:'COVERLY',BUSINESS_NAME:'[LEGAL BUSINESS NAME]',SUPPORT_EMAIL:'[SUPPORT EMAIL]',SUPPORT_PHONE:'[SUPPORT PHONE]',BUSINESS_ADDRESS:'[BUSINESS ADDRESS]',GRIEVANCE_OFFICER:'[GRIEVANCE OFFICER NAME]',GRIEVANCE_EMAIL:'[GRIEVANCE EMAIL]',GRIEVANCE_PHONE:'[GRIEVANCE PHONE]'};
+/* Public storefront configuration. */
+window.COVERLY_CONFIG = {
+  MERCHANT_UPI_ID: 'paytm.s1i6534@pty',
+  MERCHANT_NAME: 'COVERLY',
+  BUSINESS_NAME: 'Coverly Store',
+  SUPPORT_EMAIL: 'support@mycoverly.store',
+  SUPPORT_PHONE: '+91 8650635775',
+  BUSINESS_ADDRESS: 'South Park Road, Bangalore – 560020, Karnataka',
+  GRIEVANCE_OFFICER: 'Grievance Officer',
+  GRIEVANCE_EMAIL: 'support@mycoverly.store',
+  GRIEVANCE_PHONE: '+91 8650635775'
+};
+
